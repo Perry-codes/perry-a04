4cb88ae45e89f89072e58464e47b7a8693de9ce6
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-15 00:42:42 -0400

    ex45 UML and pseudo

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class Solution45 {
+
+    public static void main(String[] args) {
+
+        /*
+        Given an input file named `exercise45_input.txt`, read the file and look for all occurrences of the word utilize.
+        Replace each occurrence with use. Write the modified file to a new file.
+
+        Constraints
+        • Prompt for the name of the output file.
+        • Write the output to a new file.
+         */
+    }
+}
