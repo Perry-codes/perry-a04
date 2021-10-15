570812d526e8deaaf27c5cee2d68afce9510ef46
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-15 00:06:18 -0400

    ex44 UML and pseudo

---
+++ exercise44/src/main/java/baseline/Products.java
@@ -0,0 +1,19 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Products {
+
+    private List<String> products = new ArrayList<>();
+
+    public void Products(String inputFile){
+        //read file text as string
+        //parse data from json file into ListArray
+        //products = ListArray
+    }
+
+
+
+
+}
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,20 @@
+package baseline;
+
+public class Solution44 {
+
+    public static void main(String[] args) {
+
+
+        /*
+        Pulling data from a file into a complex data structure makes parsing much simpler. Many programming
+        languages support the JSON format, a popular way of representing data.
+        Create a program that takes a product name as input and retrieves the current price and quantity for that
+        product. The product data is in a data file in the JSON format and looks like this (you will create this file as
+        `exercise44_input.json`):
+
+        The file is in the JSON format. Use a JSON parser to pull the values out of the file (e.g. https://github.com
+        /google/gson (https://github.com/google/gson) ).
+        • If no record is found, prompt again.
+         */
+    }
+}
