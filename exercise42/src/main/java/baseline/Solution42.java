e13b2d633f01597e476695811aa2ba0dd041f79b
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-14 22:42:00 -0400

    ex42 UML and pseudo

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,37 @@
+package baseline;
+
+import java.nio.file.Files;
+
+public class Solution42 {
+
+    public static void main(String[] args) {
+
+        //set input file
+        //start output table
+        //header and ---- line
+        //%10s formatted output for even spaced columns 30 - to match
+        //send file to reader
+    }
+
+    public void readValues(Files file){
+        //while has next
+        //read line from scanner
+        //send line to split method
+        //split(input);
+        //send returned array to output
+    }
+
+    public String[] split(String fullString){
+        //take single string
+        //split at commas
+        //get 3 resulting strings
+        return null; //return array of 3 strings
+    }
+
+    private void writeTable(String[] stringArray){
+        //write strings to table
+        //%10s formatted output for even spaced columns
+    }
+
+
+}
---
+++ exercise42/src/test/java/baseline/Solution42Test.java
@@ -0,0 +1,18 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution42Test {
+
+    @Test
+    void readValues() {
+        
+    }
+
+    @Test
+    void split(){
+
+    }
+}
